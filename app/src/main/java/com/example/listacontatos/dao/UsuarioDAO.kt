3e1c42adca38e1b5cdca739fde285115d9e8a051
MauroRaya <maurorayabr@gmail.com>
2025-09-07 19:01:29 -0300

    feat: add usuario dao

---
+++ app/src/main/java/com/example/listacontatos/dao/UsuarioDAO.kt
@@ -0,0 +1,25 @@
+package com.example.listacontatos.dao
+
+import com.example.listacontatos.model.Usuario
+
+class UsuarioDAO {
+    companion object {
+        val usuarios = mutableListOf<Usuario>()
+    }
+
+    fun get(): MutableList<Usuario> {
+        return usuarios
+    }
+
+    fun has(whatsapp: String): Boolean {
+        return usuarios.any { it.whatsapp === whatsapp }
+    }
+
+    fun add(usuario: Usuario) {
+        if (has(usuario.whatsapp)) {
+            throw IllegalArgumentException("Usuário com WhatsApp ${usuario.whatsapp} já existe")
+        }
+
+        usuarios.add(usuario)
+    }
+}
